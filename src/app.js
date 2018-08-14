a62f6e99f081acc0cc380890fa6c95401328b455
Mayurgupta3 <mgupta7042@gmail.com>
2018-08-14 11:02:30 +0530

    Updated file

--- src/app.js
+++ src/app.js
@@ -1,15 +1,21 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import styles from './styles.module.css'
+import 'font-awesome/css/font-awesome.css'
+import './app.css'
+import './app.css'
 
+import App from 'containers/App/App'
 
 const App = React.createClass({
   render: function() {
-  	return (
-    	<div className={styles['container']}>
-        Text text text
-  		</div>
-  	);
+    return (
+      <div className={styles.wrapper}>
+        <h1>
+          <i className="fa fa-star"></i>
+          Environment: {__NODE_ENV__}</h1>
+      </div>
+    )
   }
 });
 
