86fa9bd412a56d293b71412ac3a8f649ad49ded8
Mayurgupta3 <mgupta7042@gmail.com>
2018-08-26 13:16:22 +0530

    With google maps class

--- src/views/Main/Container.js
+++ src/views/Main/Container.js
@@ -1,13 +1,48 @@
 import React from 'react'
+import Map, {GoogleApiWrapper} from 'google-maps-react'
+import {searchNearby} from 'utils/googleApiHelpers'
 
 export class Container extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      places: [],
+      pagination: null
+    }
+  } 	
+  onReady(mapProps, map) {
+    const {google} = this.props;
+    const opts = {
+      location: map.center,
+      radius: '500',
+      types: ['cafe']
+    }
+    searchNearby(google, map, opts)
+      .then((results, pagination) => {
+      	this.setState({
+          places: results,
+          pagination
+      }).catch((status, result) => {
+        // There was an error
+      })
+  }
   render() {
     return (
       <div>
         Hello from the container
+        <Map
+          onReady={this.onReady.bind(this)}
+          google={this.props.google} />
       </div>
     )
   }
 }
 
+export default GoogleApiWrapper({
+  apiKey: __GAPI_KEY__
+})(Container)
+
 export default Container
+
+
